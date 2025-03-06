a420c012c75f4ea9f4516fd30a978bf6f7535e62
iambraden <iambradencollins@outlook.com>
2025-03-06 15:10:02 -0800

    Animated coin counter

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,22 +1,20 @@
-using TMPro;
 using UnityEngine;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
-    [SerializeField] private int score = 0;
-    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private int score;
+    [SerializeField] private CoinCounterUI coinCounter;
 
     protected override void Awake()
     {
         base.Awake();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        scoreText = GetComponent<TextMeshProUGUI>();
     }
 
     public void IncreaseScore()
     {
         score++;
-        scoreText.text = $"Score: {score}";
+        coinCounter.UpdateScore(score);
     }
 }
